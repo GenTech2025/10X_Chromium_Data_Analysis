f23b9cfc088eeb515dd9377dafb05c5673ad6e73
GenTech2025 <roysourav2023.uk@gmail.com>
2026-07-22 03:13:12 +0100

    data downloaded

--- data/ena-file-download-read_run-PRJNA722257-fastq_ftp-20260714-0320.sh
+++
@@ -1,6 +0,0 @@
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR142/027/SRR14244127/SRR14244127.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR142/030/SRR14244130/SRR14244130.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR142/032/SRR14244132/SRR14244132.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR142/029/SRR14244129/SRR14244129.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR142/028/SRR14244128/SRR14244128.fastq.gz
-wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR142/031/SRR14244131/SRR14244131.fastq.gz
--- data/re-download.sh
+++
@@ -1,4 +0,0 @@
-awk 'NR>1 {print $1}' filereport_read_run_PRJNA722257.tsv | while read SRR_ID; do
-    echo "Downloading and splitting $SRR_ID..."
-    fasterq-dump --include-technical --split-files "$SRR_ID"
-done
